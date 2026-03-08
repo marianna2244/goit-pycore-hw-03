ad4969e7ba45d9736f9c5a20840808073848c16e
marianna2244 <166319000+marianna2244@users.noreply.github.com>
2026-03-08 22:03:05 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,20 +1,15 @@
-import random
+from datetime import datetime
 
-def get_numbers_ticket(min, max, quantity):
-    if min < 1:
-        return []
-    if max > 1000:
-        return []
-    if min >= max:
-        return []
-    if quantity < 1:
-        return []
-    if quantity > (max - min + 1):
-        return []
-
-    numbers = random.sample(range(min, max + 1), quantity)
-    numbers.sort()
-    return numbers
+def get_days_from_today(date: str) -> int:
+    try:
+        given_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        return (today - given_date).days
+    except ValueError:
+        return None
 
 
-print(get_numbers_ticket(1, 49, 6))
+print(get_days_from_today("2021-10-09"))
+print(get_days_from_today("2020-01-01"))
+print(get_days_from_today("2099-12-31"))
+print(get_days_from_today("не-дата"))
